122c2bc19550f796185dacce32b9fc4dc4a43420
ElMarto12 <m10danys@gmail.com>
2024-08-28 00:54:49 +0300

    Project Setup

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            
+            driver.Navigate().GoToUrl("https://magento.softwaretestingboard.com");
+            
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(8000);
+        }
+    }
+}
